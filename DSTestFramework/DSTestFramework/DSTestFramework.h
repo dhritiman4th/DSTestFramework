74506fc2625bffe28ca66b8deddb11cfea74ef22
dhritiman4th <dhritiman4th@gmail.com>
2023-01-01 16:50:21 +0530

    Initial commit

---
+++ DSTestFramework/DSTestFramework/DSTestFramework.h
@@ -0,0 +1,18 @@
+//
+//  DSTestFramework.h
+//  DSTestFramework
+//
+//  Created by Dhritiman Saha on 01/01/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DSTestFramework.
+FOUNDATION_EXPORT double DSTestFrameworkVersionNumber;
+
+//! Project version string for DSTestFramework.
+FOUNDATION_EXPORT const unsigned char DSTestFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DSTestFramework/PublicHeader.h>
+
+
---
+++ DSTestFramework/DSTestFramework/Service.swift
@@ -0,0 +1,16 @@
+//
+//  Service.swift
+//  DSTestFramework
+//
+//  Created by Dhritiman Saha on 01/01/23.
+//
+
+import Foundation
+
+public class Service {
+    private init() {}
+    
+    public static func doSomething() {
+        print("Hey bro!!!......I am being displayed from your Framework buddy")
+    }
+}
